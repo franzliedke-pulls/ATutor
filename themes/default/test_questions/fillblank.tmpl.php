1780069100321435077cda108e4b5c180c9d8ee8
Franz Liedke <franz@develophp.org>
2014-06-12 10:28:59 +0200

    Small template changes.

--- themes/default/test_questions/fillblank.tmpl.php
+++ themes/default/test_questions/fillblank.tmpl.php
@@ -1,3 +1,3 @@
 <p><?php echo AT_print($this->row['question'], 'tests_questions.quotesNotConverted'); ?></p>
 
-<input type="text" name="blanks[1]" value="" id="blank_<?php echo $this->row['question_id']; ?>_1">
+<input type="text" name="blanks[<?php echo $this->row['question_id']; ?>]" value="" id="blank_<?php echo $this->row['question_id']; ?>" />
